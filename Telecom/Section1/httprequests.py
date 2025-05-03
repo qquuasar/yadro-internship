dba8b3268857d8d4e31e62a868058924d71b2512
paradisekq <bigbogdaxa@gmail.com>
2025-05-03 15:34:36 +0300

    Fixed HTTP request

--- Telecom/Section1/httprequests.py
+++ Telecom/Section1/httprequests.py
@@ -2,37 +2,27 @@ import requests
 import sys
 import random
 
-
 def doRequest(url):
     try:
         response = requests.get(url)
         status_code = response.status_code
         text = response.text
-        if (status_code>=100) and (status_code < 400):
+        
+        if 100 <= status_code < 400:
+            print(f"URL: {url} | Статус: {status_code} | Ответ: {text}")
+        else:
+            print(f"URL: {url} | Ошибка: {status_code} | Ответ: {text}", file=sys.stderr)
             
-            print(f"Статус код: {status_code}, Тело ответа: {text}")
-
-        elif (status_code>=400) and (status_code<600):
-            if status_code==500: text = "Null code text"
-            raise Exception(f"Ошибка, статус код ошибки: {status_code}, Тело ответа: {text}")
-
-        return response 
-    except requests.exceptions.RequestException as exc:
-        raise Exception(f"Ошибка запроса: {str(exc)}")
-    
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"URL: {url} | Ошибка соединения: {str(e)}", file=sys.stderr)
 
 def main():
-    urls = []
-    for i in range(5):
-        appendCode = random.randint(100,599)
-        urls.append(f"https://httpstat.us/{appendCode}")
 
+    urls = [f"https://httpstat.us/{random.randint(100,599)}" for i in range(5)]
+    
     for url in urls:
-        try:
-            print(f"Запрос: {url}")
-            doRequest(url)
-        except Exception as exc:
-            print(exc, file=sys.stderr)
-
+        doRequest(url)
+        
 if __name__ == "__main__":
     main()
